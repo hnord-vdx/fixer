f539130527f30b7895f47358a0bdd9877aa139cf
Henrik Nordgren <77230891+hnord-vdx@users.noreply.github.com>
2022-03-17 22:35:04 +0100

    Update client.go

--- client.go
+++ client.go
@@ -113,7 +113,7 @@ func (c *Client) At(ctx context.Context, t time.Time, attributes ...url.Values) 
 }
 
 // Supported currencies (symbol and name)
-func (c *Client) Supported(ctx context.Context) (*Response, error) {
+func (c *Client) SupportedSymbols(ctx context.Context) (*Response, error) {
 	return c.get(ctx, "/symbols")
 }
 
